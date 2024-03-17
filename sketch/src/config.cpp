785467e5a7a7209e93073293a6853573d7375b5a
Rhian Rodrigues Barbosa <rhianbarbosa2003@gmail.com>
2024-03-17 16:23:33 -0300

    Adicao dos codigos do botao de reset e de struct para medicoes

--- sketch/src/config.cpp
+++ sketch/src/config.cpp
@@ -1,10 +1,10 @@
 #include "../include/config.hpp"
 
-// Inicializando o lcd
+// Inicializando o objeto do ldc
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-// Inicializando o rtc
+// Inicializando o objeto do rtc
 RTC_DS3231 rtc;
 
-// Inicializando o dht
+// Inicializando o objeto do dht
 DHT dht(DHT_PIN, DHT11);
