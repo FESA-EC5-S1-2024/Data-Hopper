e9dfecd8e2292712b3879585497abe185b2a4da2
Roger Freitas Pereira <roger_freitas@live.com>
2024-03-17 10:06:45 -0300

    Criação do arquivo logo.cpp, movendo assim as variáveis de logo.hpp para logo.cpp
    Criação do arquivo logo.cpp na pasta /sketch/src/ para receber as variáveis da logo.hpp da pasta /sketch/include/. Essa reorganização permite que o código relacionado ao logo seja melhor estruturado.

--- sketch/include/logo.hpp
+++ sketch/include/logo.hpp
@@ -1,24 +1,3 @@
-// Desenho da Apresentação
-byte grasshopper1[] = {B00000, B00000, B00000, B00000,
-                       B00000, B01111, B10001, B01111};
+#pragma once
 
-byte grasshopper2[] = {B00000, B00000, B00000, B00000,
-                       B11100, B11010, B10001, B01000};
-
-byte grasshopper3[] = {B00000, B00000, B00000, B00000,
-                       B00000, B00001, B11111, B00000};
-
-byte grasshopper4[] = {B00010, B00100, B01000, B11110,
-                       B00001, B00001, B00010, B11100};
-
-byte grasshopper5[] = {B00110, B00100, B00101, B00101,
-                       B00101, B00101, B01010, B11100};
-
-byte grasshopper6[] = {B01111, B10000, B01110, B00001,
-                       B00000, B00000, B00000, B00000};
-
-byte grasshopper7[] = {B11001, B01000, B00100, B11010,
-                       B00111, B00000, B00000, B00000};
-
-byte grasshopper8[] = {B01000, B01000, B01000, B10000,
-                       B11000, B10110, B01001, B00110};
+#include "Arduino.h"
