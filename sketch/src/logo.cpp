c5ad1a9d689450180f236bd6fc18042c142d5b5d
Roger Freitas Pereira <roger_freitas@live.com>
2024-03-17 10:49:41 -0300

    Formatando o arquivo com clang-format

--- sketch/src/logo.cpp
+++ sketch/src/logo.cpp
@@ -26,22 +26,22 @@ byte grasshopper7[] = {0b11001, 0b01000, 0b00100, 0b11010,
 byte grasshopper8[] = {0b01000, 0b01000, 0b01000, 0b10000,
                        0b11000, 0b10110, 0b01001, 0b00110};
 
-void entradaEmpresa(){
+void entradaEmpresa() {
 
-  lcd.setCursor(2,0);
+  lcd.setCursor(2, 0);
 
-  for(int i = 0; i < 4; i++){
+  for (int i = 0; i < 4; i++) {
     lcd.print(nomeEmpresa[i]);
     delay(100);
   }
 
-  lcd.setCursor(2,1);
+  lcd.setCursor(2, 1);
 
-  for(int i = 4; i < 10; i++){
+  for (int i = 4; i < 10; i++) {
     lcd.print(nomeEmpresa[i]);
     delay(100);
   }
-  
+
   lcd.createChar(0, grasshopper1);
   lcd.createChar(1, grasshopper2);
   lcd.createChar(2, grasshopper3);
@@ -53,29 +53,28 @@ void entradaEmpresa(){
 
   lcd.setCursor(9, 0);
   lcd.write(byte(0));
-  
+
   lcd.setCursor(10, 0);
   lcd.write(byte(1));
-  
+
   lcd.setCursor(11, 0);
   lcd.write(byte(2));
-  
+
   lcd.setCursor(12, 0);
   lcd.write(byte(3));
-  
+
   lcd.setCursor(9, 1);
   lcd.write(byte(4));
-  
+
   lcd.setCursor(10, 1);
   lcd.write(byte(5));
-  
+
   lcd.setCursor(11, 1);
   lcd.write(byte(6));
-  
+
   lcd.setCursor(12, 1);
   lcd.write(byte(7));
 
   delay(2500);
   lcd.clear();
 }
-
