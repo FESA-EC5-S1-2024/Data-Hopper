4ccd3ba1213c7b0fbe4221981a96cee6380e1501
Roger Freitas Pereira <roger_freitas@live.com>
2024-03-17 10:36:29 -0300

    Movendo a função de imprimir o logo da empresa para seu respeqctivo arquivo
    Move a função 'entradaEmpresa()' do arquivo 'sketch.ino' para o arquivo 'logo.cpp', adicionando suas respectivas dependencias.

--- sketch/src/logo.cpp
+++ sketch/src/logo.cpp
@@ -1,3 +1,4 @@
+#include "../include/config.hpp"
 #include "../include/logo.hpp"
 
 // Desenho da Apresentação
@@ -24,3 +25,57 @@ byte grasshopper7[] = {0b11001, 0b01000, 0b00100, 0b11010,
 
 byte grasshopper8[] = {0b01000, 0b01000, 0b01000, 0b10000,
                        0b11000, 0b10110, 0b01001, 0b00110};
+
+void entradaEmpresa(){
+
+  lcd.setCursor(2,0);
+
+  for(int i = 0; i < 4; i++){
+    lcd.print(nomeEmpresa[i]);
+    delay(100);
+  }
+
+  lcd.setCursor(2,1);
+
+  for(int i = 4; i < 10; i++){
+    lcd.print(nomeEmpresa[i]);
+    delay(100);
+  }
+  
+  lcd.createChar(0, grasshopper1);
+  lcd.createChar(1, grasshopper2);
+  lcd.createChar(2, grasshopper3);
+  lcd.createChar(3, grasshopper4);
+  lcd.createChar(4, grasshopper5);
+  lcd.createChar(5, grasshopper6);
+  lcd.createChar(6, grasshopper7);
+  lcd.createChar(7, grasshopper8);
+
+  lcd.setCursor(9, 0);
+  lcd.write(byte(0));
+  
+  lcd.setCursor(10, 0);
+  lcd.write(byte(1));
+  
+  lcd.setCursor(11, 0);
+  lcd.write(byte(2));
+  
+  lcd.setCursor(12, 0);
+  lcd.write(byte(3));
+  
+  lcd.setCursor(9, 1);
+  lcd.write(byte(4));
+  
+  lcd.setCursor(10, 1);
+  lcd.write(byte(5));
+  
+  lcd.setCursor(11, 1);
+  lcd.write(byte(6));
+  
+  lcd.setCursor(12, 1);
+  lcd.write(byte(7));
+
+  delay(2500);
+  lcd.clear();
+}
+
